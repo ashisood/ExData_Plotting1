194b438b19137e6456ed6cde71644e83723a1026
Ashish Sood <sood_303@yahoo.com>
2017-03-18 17:34:07 -0700

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -7,7 +7,7 @@ subSetmydfData <- filter(mydfdata, Date == "1/2/2007" | Date == "2/2/2007" )
 
 #str(subSetData)
 globalActivePower <- as.numeric(subSetmydfData$Global_active_power)
-#png("plot1.png", width=480, height=480)
+png("plot1.png", width=480, height=480)
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-#dev.off()
+dev.off()
 
